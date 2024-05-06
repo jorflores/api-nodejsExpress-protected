a2c92e2f8b477682b1147a424ba6c5bbff6e120c
Jorge Flores <flores.jorgeluis@gmail.com>
2024-05-06 12:06:35 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const userRoutes = require("./routes/routeuser");
+const orgRoutes = require("./routes/routeorg");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const MONGO_DB =
+  process.env.MONGO_DB || "mongodb://127.0.0.1:27017/tc2007b_404";
+dbConnect();
+
+app.use(express.json());
+app.use("/users", userRoutes);
+app.use("/orgs/", orgRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Servidor ejecutandose en el puerto: ${PORT}`);
+});
+
+function dbConnect() {
+  if (process.env.MONGO_DB) {
+    console.log("Connecting to MongoDB in the cloud!");
+  } else {
+    console.log(`Connecting to Local instance of MongoDB : ${MONGO_DB}`);
+  }
+
+  mongoose.connect(MONGO_DB).then(() => console.log("Connected to DB!"));
+}
---
+++ routes/routeuser.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const app = express();
+const jwt = require("jsonwebtoken");
+const User = require("../model/user");
+const verifyToken = require("../middleware/verify");
+const bcrypt = require("bcrypt");
+
+// Registration Endpoint
+app.post("/register", async (req, res) => {
+  try {
+    const { phoneNumber, password } = req.body;
+
+    const existingUser = await User.findOne({ phoneNumber });
+
+    if (existingUser) {
+      return res.json({ message: "El teléfono ya se encuentra registrado" });
+    }
+
+    let hashed_password = bcrypt.hashSync(password, 10);
+
+    const newUser = new User({
+      phoneNumber: phoneNumber,
+      password: hashed_password,
+    });
+    await newUser.save();
+
+    res.status(201).json({ message: "Registro exitoso" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error" });
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const { phoneNumber, password } = req.body;
+
+    // Revisamos si el usuario existe.
+    const user = await User.findOne({ phoneNumber });
+
+    // Revisamos si el usuario no existe y si la contraseña no es la misma
+    if (!user || !bcrypt.compareSync(password, user.password)) {
+      return res.status(401).json({ message: "Credenciales incorrectas" });
+    }
+
+    // Generamos el token
+    const token = jwt.sign(
+      { phoneNumber: user.phoneNumber, userId: user._id },
+      "your-secret-key",
+      {
+        expiresIn: "1h",
+      }
+    );
+
+    res.status(200).json({ token: token, isAdmin: user.isAdmin });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error" });
+  }
+});
+
+app.get("/protected", verifyToken, (req, res) => {
+  console.log(req.user.userId);
+  res.status(200).json({ message: "Acceso permitido" });
+});
+
+module.exports = app;
